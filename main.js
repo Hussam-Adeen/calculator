8df4e482b47b64bc42096c7b021a3c3ed575a686
Hussam-Adeen <91805374+Hussam-Adeen@users.noreply.github.com>
2021-10-03 22:56:10 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+function sum0(){
+                  var x = document.getElementById("x");
+                  var y = document.getElementById("y");
+                  var z = document.getElementById("z");
+                  
+  z.innerHTML=parseInt(x.value) + parseInt(y.value)
+}
+
+
+function sum1() {
+                  var x = document.getElementById("x");
+                  var y = document.getElementById("y");
+                  var z = document.getElementById("z");
+
+  z.innerHTML=parseInt(x.value) - parseInt(y.value)
+}
+
+
+function sum2() {
+                  var x = document.getElementById("x");
+                  var y = document.getElementById("y");
+                  var z = document.getElementById("z");
+
+  z.innerHTML = parseInt(x.value) / parseInt(y.value)
+}
+
+
+
+function sum3() {
+                  var x = document.getElementById("x");
+                  var y = document.getElementById("y");
+                  var z = document.getElementById("z");
+
+  z.innerHTML=parseInt(x.value) * parseInt(y.value)
+}
+
